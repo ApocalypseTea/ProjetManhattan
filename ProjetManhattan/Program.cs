79af95795500325a07efe0c9e2b1d14d5b1b38c5
AdeLas <AdeLas@DESKTOP-MI78SVG>
2025-02-20 15:41:40 +0100

    Refactoring : renommage de variable et precision sur index de tableau. Nettoyage de commentaires de test. REattribution et types pour la classe Ligne de Log

--- ProjetManhattan/Program.cs
+++ ProjetManhattan/Program.cs
@@ -5,22 +5,38 @@ namespace ProjetManhattan
 {        
     class LigneDeLog
     {
-        DateTime dateHeure;
-        string ipSource;
-        verbesHTTP methodeHTTP; 
-        string csUriStem;
-        string csUriQuery;
-        int port;
-        string username;
-        string ipClient;
-        string userAgentHTTP;
-        string referer;
-        string csStatutHTTP;
-        string csSubStatut;
-        string scWin32Status;
-        int timeTaken;
+        private const int COL_DATE = 0;
+        private const int COL_TIME = 1;
+        private const int COL_IP_SOURCE = 2;
+        private const int COL_METHODE_HTTP = 3;
+        private const int COL_CS_URI_STEM = 4;
+        private const int COL_URI_QUERY = 5;
+        private const int COL_PORT = 6;
+        private const int COL_USERNAME = 7;
+        private const int COL_IP_CLIENT = 8;
+        private const int COL_USER_AGENT_HTTP = 9;
+        private const int COL_REFERER = 10;
+        private const int COL_CS_STATUS_HTTP = 11;
+        private const int COL_CS_SUBSTATUS = 12;
+        private const int COL_CS_WIN32_STATUS = 13;
+        private const int COL_TIME_TAKEN = 14;
 
-        public enum verbesHTTP
+        private DateTime dateHeure;
+        private string ipSource;
+        private VerbesHTTP methodeHTTP; 
+        private string csUriStem;
+        private string csUriQuery;
+        private int port;
+        private string username;
+        private string ipClient;
+        private string userAgentHTTP;
+        private string referer;
+        private int csStatutHTTP;
+        private int csSubStatut;
+        private int csWin32Status;
+        private int timeTaken;
+
+        public enum VerbesHTTP
         {
             GET,
             POST,
@@ -36,20 +52,20 @@ namespace ProjetManhattan
         //Constructeur - prend un tableau de string en argument
         public LigneDeLog(string [] champsLog) 
         {
-            this.dateHeure = DateTime.Parse(champsLog[0] + " " + champsLog[1]);
-            this.ipSource = champsLog[2];
-            this.methodeHTTP = (verbesHTTP)Enum.Parse(typeof(verbesHTTP), champsLog[3]);
-            this.csUriStem = champsLog[4];
-            this.csUriQuery = champsLog[5];
-            this.port = int.Parse(champsLog[6]);
-            this.username = champsLog[7];
-            this.ipClient = champsLog[8];
-            this.userAgentHTTP = champsLog[9];
-            this.referer = champsLog[10];
-            this.csStatutHTTP = champsLog[11];
-            this.csSubStatut = champsLog[12];
-            this.scWin32Status = champsLog[13];
-            this.timeTaken = int.Parse(champsLog[14]);
+            this.dateHeure = DateTime.Parse(champsLog[COL_DATE] + " " + champsLog[COL_TIME]);
+            this.ipSource = champsLog[COL_IP_SOURCE];
+            this.methodeHTTP = (VerbesHTTP)Enum.Parse(typeof(VerbesHTTP), champsLog[COL_METHODE_HTTP]);
+            this.csUriStem = champsLog[COL_URI_QUERY];
+            this.csUriQuery = champsLog[COL_URI_QUERY];
+            this.port = int.Parse(champsLog[COL_PORT]);
+            this.username = champsLog[COL_USERNAME];
+            this.ipClient = champsLog[COL_IP_CLIENT];
+            this.userAgentHTTP = champsLog[COL_USER_AGENT_HTTP];
+            this.referer = champsLog[COL_REFERER];
+            this.csStatutHTTP = int.Parse(champsLog[COL_CS_STATUS_HTTP]);
+            this.csSubStatut = int.Parse(champsLog[COL_CS_SUBSTATUS]);
+            this.csWin32Status = int.Parse(champsLog[COL_CS_WIN32_STATUS]);
+            this.timeTaken = int.Parse(champsLog[COL_TIME_TAKEN]);
         }
 
         public void AfficherLigneLog()
@@ -82,17 +98,11 @@ namespace ProjetManhattan
                     continue;
                 } 
                 else
-                {
-                    
+                {                    
                     string[] champs = ligne.Split(' ');
                     LigneDeLog ligneLog = new LigneDeLog(champs);
                     ligneLog.AfficherLigneLog();
 
-                    /*for (int i = 0; i < champs.Length; i++)
-                    {
-                        Console.WriteLine(champs[i]);
-                        
-                    }*/
                     Console.WriteLine();
                 }
                 
