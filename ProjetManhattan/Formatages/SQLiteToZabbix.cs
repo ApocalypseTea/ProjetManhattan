a72f25d2ae954f5765700ac2aac94ffa36df3fe7
Adelas <lassalle.adeline@protonmail.com>
2025-04-09 14:31:52 +0200

    Bug Fixes : Correction de la requete SQLIteToZabbix.GetValueFromTraitementTargetPropertyName et de la requete principale (correction typo)

--- ProjetManhattan/Formatages/SQLiteToZabbix.cs
+++ ProjetManhattan/Formatages/SQLiteToZabbix.cs
@@ -14,7 +14,7 @@ namespace ProjetManhattan.Formatages
 {
     class SQLiteToZabbix
     {
-        private const string QUERY = "SELECT DISTINCT traitement, target, _date, propertyName, description FROM _record WHERE traitement=@traitement AND _date BETWEEN @debutExport AND @finExport;";
+        private const string QUERY = "SELECT DISTINCT traitement, target, date, propertyName, description FROM record WHERE traitement=@traitement AND date BETWEEN @debutExport AND @finExport;";
         private List<ZabbixData> _zabbixListe;
         private SqliteConnection _connection;
         private DateTime _dateDebutExport;
@@ -51,7 +51,7 @@ namespace ProjetManhattan.Formatages
                     int colTarget = reader.GetOrdinal("target");
                     int colPropertyName = reader.GetOrdinal("propertyName");
                     int colDescription = reader.GetOrdinal("description");
-                    int colDate = reader.GetOrdinal("_date");
+                    int colDate = reader.GetOrdinal("date");
 
                     string traitement = reader.GetString(colTraitement);
                     string target = reader.GetString(colTarget);
@@ -73,7 +73,7 @@ namespace ProjetManhattan.Formatages
         }
         public string GetValueFromTraitementTargetPropertyName (string nomTraitement, string nomTarget, string nomPropertyName)
         {
-            string query = "SELECT value FROM _record WHERE traitement = @traitement AND target = @target AND propertyName = @propertyName GROUP BY _date HAVING _date = MAX(_date);";
+            string query = "SELECT value FROM record WHERE traitement = @traitement AND target = @target AND propertyName = @propertyName GROUP BY date HAVING date = MAX(date);";
             SqliteCommand requete = new SqliteCommand(query, _connection);
 
             requete.Parameters.AddWithValue("@traitement", nomTraitement);
